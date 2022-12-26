84d87739165d6e12e33905396355e7dd4e056846
xiongxinx <xiongxin_xiao@trendmicro.com>
2022-12-26 18:39:32 -0500

    Blog project done

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,41 +1,58 @@
+import { useMutation, useQuery } from "react-query";
+
 async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
-  return response.json();
+	const response = await fetch(
+		`https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+	);
+	return response.json();
 }
 
 async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
-  return response.json();
+	const response = await fetch(
+		`https://jsonplaceholder.typicode.com/postId/${postId}`,
+		{ method: "DELETE" }
+	);
+	return response.json();
 }
 
 async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
-  return response.json();
+	const response = await fetch(
+		`https://jsonplaceholder.typicode.com/postId/${postId}`,
+		{ method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+	);
+	return response.json();
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+	// replace with useQuery
+	const { data, isLoading } = useQuery(["comments", post.id], () =>
+		fetchComments(post.id)
+	);
 
-  return (
-    <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
-      <p>{post.body}</p>
-      <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
-      ))}
-    </>
-  );
+	const deleteMutation = useMutation((postId) => deletePost(postId));
+
+	const updateMutation = useMutation((postId) => updatePost(postId));
+
+	if (isLoading) return <h3>Loading...</h3>;
+
+	return (
+		<>
+			<h3 style={{ color: "blue" }}>{post.title}</h3>
+			<button onClick={() => deleteMutation.mutate(post.id)}>
+				Delete
+			</button>
+			{deleteMutation.isSuccess && <h3>delete success</h3>}
+			{updateMutation.isSuccess && <h3>update success</h3>}
+			<button onClick={() => updateMutation.mutate(post.id)}>
+				Update title
+			</button>
+			<p>{post.body}</p>
+			<h4>Comments</h4>
+			{data.map((comment) => (
+				<li key={comment.id}>
+					{comment.email}: {comment.body}
+				</li>
+			))}
+		</>
+	);
 }
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,46 +1,83 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { PostDetail } from "./PostDetail";
+
+import { useQueryClient, useQuery } from "react-query";
+
 const maxPostPage = 10;
 
-async function fetchPosts() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
-  );
-  return response.json();
+async function fetchPosts(pageNum) {
+	const response = await fetch(
+		`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
+	);
+	return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [selectedPost, setSelectedPost] = useState(null);
+	const [currentPage, setCurrentPage] = useState(0);
+	const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+	const queryClient = useQueryClient();
 
-  return (
-    <>
-      <ul>
-        {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
-            {post.title}
-          </li>
-        ))}
-      </ul>
-      <div className="pages">
-        <button disabled onClick={() => {}}>
-          Previous page
-        </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
-          Next page
-        </button>
-      </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
-    </>
-  );
+	console.log("rerender");
+
+	useEffect(() => {
+		if (currentPage < maxPostPage) {
+			const nextPage = currentPage + 1;
+			queryClient.prefetchQuery(
+				["posts", nextPage],
+				() => fetchPosts(nextPage),
+				{
+					staleTime: 300000,
+				}
+			);
+		}
+	}, [currentPage, queryClient]);
+
+	// replace with useQuery
+	const { data, isFetching } = useQuery(
+		["posts", currentPage],
+		() => fetchPosts(currentPage),
+		{
+			staleTime: 300000,
+			keepPreviousData: true,
+		}
+	);
+	if (isFetching) return <h3>Loading...</h3>;
+	return (
+		<>
+			<ul>
+				{data.map((post) => (
+					<li
+						key={post.id}
+						className="post-title"
+						onClick={() => setSelectedPost(post)}
+					>
+						{post.title}
+					</li>
+				))}
+			</ul>
+			<div className="pages">
+				<button
+					disabled={currentPage <= 1}
+					onClick={() => {
+						setCurrentPage((previousValue) => previousValue - 1);
+					}}
+				>
+					Previous page
+				</button>
+				<span>Page {currentPage}</span>
+				<button
+					disabled={currentPage >= maxPostPage}
+					onClick={() => {
+						setCurrentPage((previousValue) => previousValue + 1);
+					}}
+				>
+					Next page
+				</button>
+			</div>
+			<hr />
+			{selectedPost && <PostDetail post={selectedPost} />}
+		</>
+	);
 }
